eecbb8f45fc4d87dadc3b99d5ad993f08beb3a71
negri234279 <rafalejo@live.com>
2023-02-24 15:04:12 +0100

    Make group for routes privates with Auth

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -61,11 +61,7 @@ class AuthController extends Controller
 
     public function dashboard()
     {
-        if (Auth::check()) {
-            return view('dashboard');
-        }
-
-        return redirect("login")->withSuccess('You are not allowed to access');
+        return view('dashboard');
     }
 
     public function signOut()
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn() => view('dashboard'));
-
-Route::get('/dashboard', [AuthController::class, 'dashboard']);
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
 Route::get('/register', [AuthController::class, 'register'])->name('register-user');
 Route::post('/custom-registration', [AuthController::class, 'customRegistration'])->name('register.custom');
 Route::get('/signout', [AuthController::class, 'signOut'])->name('signout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', fn() => view('dashboard'));
+    Route::get('/dashboard', [AuthController::class, 'dashboard']);
+});
